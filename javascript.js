45743022367a099e6729be6fccdfa0138469a38e
mohosin2126 <mohosin212@gmail.com>
2023-09-21 01:23:56 +0800

    project done

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const loadPhone = async(searchText,show)=>{
+const loadPhone = async(searchText="iphone",show)=>{
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json()
     const phone = data.data
@@ -23,8 +23,8 @@ const displayPhone=(phones,show)=>{
 const phoneCard=document.createElement("div")
 phoneCard.innerHTML=`
  
- <div class="card w-96 bg-gray-100 shadow-xl mt-10  ">
-  <figure><img src="${phone.image}" /></figure>
+ <div class="card md:w-96 bg-gray-100 shadow-xl mt-10  ">
+  <figure><img class="p-4 md:p-2" src="${phone.image}" /></figure>
   <div class="card-body">
     <h2 class="text-center text-xl font-semibold mb-2">${phone.phone_name}</h2>
     <div class="card-actions justify-center mt-2">
@@ -65,6 +65,24 @@ const handleSearch=(show)=>{
 const handleDetails= async(id)=>{
     const res= await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
     const data =await res.json()
+    showModal(data.data)
+}
+
+const showModal=(phone)=>{
+    const showDetailsPhoneName=document.getElementById("show-details-phone-name")
+    showDetailsPhoneName.innerText=phone.name
+    const phoneDes=document.getElementById("phone-des")
+    phoneDes.innerHTML=`
+    
+    <img class="flex mx-auto mt-3" src="${phone.image}" />
+    <p class="mt-4">Storage: ${phone.mainFeatures.storage}</p> <br>
+    <p>Chipset: ${phone.mainFeatures.chipSet}</p> <br>
+    <p>Memory: ${phone.mainFeatures.memory}</p> <br>
+    
+    <p>Brand: ${phone.brand}</p> <br>
+    <p>Release Date: ${phone.releaseDate}</p> <br>
+    `
+    show_details_5.showModal()
 }
 
 loadPhone()
